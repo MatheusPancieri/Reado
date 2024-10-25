9f905b3bfdf92180d77ca53dcfcf812be02ed2fa
Matheus Pancieri Preza da Silva - Estágio COTIN <matheuspancieri@outlook.com>
2024-10-25 17:30:01 -0400

    small changes

--- Reado.Domain/obj/Debug/net9.0/Reado.Domain.AssemblyInfo.cs
+++ Reado.Domain/obj/Debug/net9.0/Reado.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reado.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3745fc1bed975dd7170074fc7582d075dd5bf931")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reado.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reado.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
